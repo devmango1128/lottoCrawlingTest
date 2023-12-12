880271837ebf96128e4be3dbb74a233ef277d0d8
devmango1128 <devmango1128@gmail.com>
2023-12-12 22:53:11 +0900

    first commit

---
+++ src/main/java/com/lottocrawlingtest/www/LottoCrawlingTestApplication.java
@@ -0,0 +1,13 @@
+package com.lottocrawlingtest.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottoCrawlingTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LottoCrawlingTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lottocrawlingtest/www/LottoCrawlingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lottocrawlingtest.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottoCrawlingTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
